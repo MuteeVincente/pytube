47ff8b0bf2c75470fdc600d22cb09ec806b02a28
MuteeVincente <vinniemutee@gmail.com>
2023-09-24 03:22:51 +0300

    Final Commit

--- py2be.py
+++ py2be.py
@@ -9,10 +9,9 @@ video_url = input('Enter url: ')
 video_instance = pytube.YouTube(video_url)
 stream = video_instance.streams.get_highest_resolution()
 
-
 #download
 
 video1= stream.download()
 
-# print(video1.title)
+print(video1.title)
 
